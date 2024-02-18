b767d332be0e35dd47ba70d161b782926d70a322
Evghenii <egx@egx-mba.lan>
2024-02-18 09:22:23 -0800

    next

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -1,1 +1,20 @@
 // first.rs
+pub enum List {
+    Empty,
+    ElemThenEmpty(i32),
+    ElemThenNotEmpty(i32, Box<List>),
+}
+
+pub struct List1 {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
